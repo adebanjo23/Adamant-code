4670a7fa79856b5fc7388aed575b7e88bf946a8e
mayowa adebajo <temidun2003ade@gmail.com>
2024-02-09 09:37:28 +0100

    Updated settings file

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-tp-^p9k-4jw6e2!@7=a3n$ckfz2a5eg5-q+p-=r3amz@hf2e%@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
