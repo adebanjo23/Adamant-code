d8698ce286296eb0a5cd4acd850fa38f5dd0db79
mayowa adebajo <temidun2003ade@gmail.com>
2024-02-09 09:55:09 +0100

    Updated settings again

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +26,7 @@ SECRET_KEY = "django-insecure-tp-^p9k-4jw6e2!@7=a3n$ckfz2a5eg5-q+p-=r3amz@hf2e%@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = 'adamant-code.onrender.com'
 
 
 # Application definition
